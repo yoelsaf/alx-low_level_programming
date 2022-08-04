1dd5255c15947407252b5632d9404798138377fe
yoel <yoelsaf@gmail.com>
2022-08-05 00:06:17 +0300

    'vardiak'

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,28 +1,25 @@
-#ifndef _VARIADIC_FUNCTIONS_H_
-#define _VARIADIC_FUNCTIONS_H_
+#ifndef VARIADIC_FUNCTIONS_H
+#define VARIADIC_FUNCTIONS_H
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <stdarg.h>
+
+/**
+ * struct argType - Struct argType
+ * @at: type of argument
+ * @f: the function associated
+ */
+
+typedef struct argType
+{
+	char *at;
+	void (*f)(va_list);
+} pType;
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
-/**
- * struct p - checker struct
- * @p: possibility
- * @f: Function to be used
- */
-typedef struct p
-{
-  char *p;
-  void (*f)(va_list);
-} checker;
 
-void print_c(va_list);
-void print_i(va_list);
-void print_f(va_list);
-void print_s(va_list);
-
-#endif
-
+#endif /* VARIADIC_FUNCTIONS_H */
