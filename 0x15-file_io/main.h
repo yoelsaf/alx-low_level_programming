cae0ceb4ad1244a5b5caaba9744f09c3688a2899
yoel <yoelsaf@gmail.com>
2022-08-22 20:26:20 +0300

    'nw_file'

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,29 +1,17 @@
 #ifndef MAIN_H
-#define MAIN_N
+#define MAIN_H
 
-#include <elf.h>
-#include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
 
+int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
+int _strlen(char *str);
 int append_text_to_file(const char *filename, char *text_content);
 
-/**
- * ELF header functions
-*/
-void check_elf(unsigned char *e_ident);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_abi(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void print_entry(unsigned long int e_entry, unsigned char *e_ident);
-void close_elf(int elf);
 #endif
